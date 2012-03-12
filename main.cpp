21beef52911b35a5b211374b0f9336646c710a43
unknown <Owen@.(none)>
2012-03-12 18:50:51 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <GL\glew.h>
+#include <GL\glfw.h>
+
+bool running = true;
+
+void update()
+{
+
+}
+
+void display()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0);
+	glBegin(GL_QUADS);
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex2f(100, 100);
+	glVertex2f(700, 100);
+	glVertex2f(700, 500);
+	glVertex2f(100, 500);
+	glEnd();
+	glfwSwapBuffers();
+}
+
+int Exit()
+{
+	running = false;
+	return 0;
+}
+
+int main(int argc, char**argv)
+{
+	if(!glfwInit())
+		return 1;
+
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 0);
+
+	if (!glfwOpenWindow(800, 600, 8, 8, 8, 8, 24, 8, GLFW_WINDOW))
+		return 1;
+
+	glfwSetWindowCloseCallback(Exit);	
+
+	while(running)
+	{
+		update();
+		display();		
+	}
+
+	glfwTerminate();
+}
